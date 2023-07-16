0878be2cab145778368573f7b1103975c8892bbc
CarlitosDroid <carlosleonardovargashuaman@gmail.com>
2023-07-15 19:37:55 -0500

    Added functions bodies samples

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,20 @@
 fun main(args: Array<String>) {
+    println(sayHello())
 }
 
-fun sayHello(): String {
-    return "Hello"
-}
+fun sayHello(): String =
+     "Hello"
+
 
 fun sum(a: Int, b: Int): Int {
     return a + b
 }
 
-fun max(a: Int, b: Int): Int {
-    return if(a > b) a else b
+fun max(a: Int, b: Int): Int =
+     if(a > b) a else b
+
+fun calculateMiddleIndex(array: IntArray): Int {
+    val start = 0
+    val end = array.size - 1
+    return start + (end - start) / 2
 }
